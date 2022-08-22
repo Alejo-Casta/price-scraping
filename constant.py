064e271af29657a4b49f28b2aedc8a6396102e50
Alejo-Casta <alejo.casta.al@gmail.com>
2022-08-22 02:56:23 -0500

    Eliminando la key

--- constant.py
+++ constant.py
@@ -18,26 +18,46 @@ def get_price_aliexpress(re, price):
 
 
 STORES = {
-    # 'https://www.falabella.com.co': {
-    #     'tag': ['[data-internet-price]', '[data-normal-price]'],
-    #     'get_price': get_price_standar
-    # },
-    # 'https://www.homecenter.com.co': {
-    #     'tag': ['.product-price .primary'],
-    #     'get_price': get_price_standar
-    # },
-    # 'https://www.exito.com': {
-    #     'tag': ['.exito-vtex-components-4-x-valuePLPAllied',
-    #             '.exito-vtex-components-4-x-PricePDP',
-    #             '.exito-vtex-components-4-x-list-price'],
-    #     'get_price': get_price_standar
-    # },
-    # 'https://es.aliexpress.com': {
-    #     'tag': ['.product-price-current'],
-    #     'get_price': get_price_aliexpress
-    # },
+    'https://www.falabella.com.co': {
+        'tag': ['[data-internet-price]', '[data-normal-price]'],
+        'get_price': get_price_standar
+    },
+    'https://www.homecenter.com.co': {
+        'tag': ['.product-price .primary'],
+        'get_price': get_price_standar
+    },
+    'https://www.exito.com': {
+        'tag': ['.exito-vtex-components-4-x-valuePLPAllied',
+                '.exito-vtex-components-4-x-PricePDP',
+                '.exito-vtex-components-4-x-list-price'],
+        'get_price': get_price_standar
+    },
+    'https://es.aliexpress.com': {
+        'tag': ['.product-price-current'],
+        'get_price': get_price_aliexpress
+    },
     'https://www.amazon.com': {
         'tag': ['.a-price-whole'],
+        'get_price': get_price_standar
+    },
+    'https://reset.net.co': {
+        'tag': ['.electro-price'],
+        'get_price': get_price_standar
+    },
+    'https://www.mercadolibre.com.co': {
+        'tag': ['.ui-pdp-price__second-line .andes-visually-hidden', '.andes-money-amount__fraction'],
+        'get_price': get_price_standar
+    },
+    'https://co.jbl.com': {
+        'tag': ['.pricing .price-sales'],
+        'get_price': get_price_standar
+    },
+    'https://btechnology.com.co': {
+        'tag': ['.price ins'],
+        'get_price': get_price_standar
+    },
+    'https://www.adidas.co': {
+        'tag': ['.gl-price-item--sale'],
         'get_price': get_price_standar
     }
 }
--- main.py
+++ main.py
@@ -30,16 +30,22 @@ def get_links_df():
 def open_browser():
     df = get_links_df()
     with sync_playwright() as p:
-        browser = p.firefox.launch_persistent_context(user_data_dir='./data/session.json')
+        browser = p.firefox.launch()
+        context = browser.new_context(storage_state='data.json')
         for i in df.index:
             link = df['Link'][i]
             if df['Shop'][i] in constant.STORES:
                 store = constant.STORES[df['Shop'][i]]
                 print(link)
-                page = browser.new_page()
+                if df['Shop'][i] == 'https://www.amazon.com':
+                    page = context.new_page()
+                else:
+                    page = browser.new_page()
                 page.goto(link)
-                page.wait_for_timeout(5000)
+                if df['Shop'][i] == 'https://www.exito.com':
+                    page.wait_for_timeout(5000)
                 for selector in store['tag']:
+                    # page.pause()
                     price_html = page.query_selector(selector)
                     if price_html:
                         price = store['get_price'](re, price_html.text_content())
